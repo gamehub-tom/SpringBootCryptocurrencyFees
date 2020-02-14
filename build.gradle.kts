fb4dd662b3c0d7522a0eaa1f602705400493ade8
Tom Lee <tom.lee@mcngamehub.com>
2020-02-14 20:41:23 +0900

    - setup coveralls

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.2.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("jacoco")
+    id("com.github.kt3k.coveralls") version "2.8.4"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
 }
@@ -10,6 +12,9 @@ plugins {
 group = "com.playcoin"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+jacoco {
+    toolVersion = "0.8.5"
+}
 
 val developmentOnly by configurations.creating
 configurations {
@@ -21,8 +26,10 @@ configurations {
     }
 }
 
+
 repositories {
     mavenCentral()
+    maven { url = uri("https://plugins.gradle.org/m2/") }
 }
 
 dependencies {
@@ -40,6 +47,7 @@ dependencies {
     }
 }
 
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
@@ -48,5 +56,34 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict", "-Xallow-result-return-type")
         jvmTarget = "1.8"
+    }
+}
+
+// kotlin DSL
+
+tasks.jacocoTestReport {
+    reports {
+        // 원하는 리포트를 켜고 끌 수 있습니다.
+        html.isEnabled = true
+        xml.isEnabled = false
+        csv.isEnabled = false
+
+//  각 리포트 타입 마다 리포트 저장 경로를 설정할 수 있습니다.
+//  html.destination = file("$buildDir/jacocoHtml")
+//  xml.destination = file("$buildDir/jacoco.xml")
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "CLASS"
+
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.90".toBigDecimal()
+            }
+        }
     }
 }
